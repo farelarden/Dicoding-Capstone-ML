b9a18c727bab740c08d1d8dd47f6427973160605
Farel Arden <farelarden13@gmail.com>
2021-12-04 22:32:55 +0700

    Add files via upload
    Here we changed the model that we're going to use from RNN(LSTM) to facebook prophet who offers a better forecasting than the last model

--- Deployment/app.py
+++ Deployment/app.py
@@ -1,67 +1,79 @@
 from flask import Flask, render_template, request
 
+import os
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-from statsmodels.tsa.seasonal import seasonal_decompose
-from sklearn.preprocessing import MinMaxScaler
-from keras.preprocessing.sequence import TimeseriesGenerator
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-import math 
+from fbprophet import Prophet
+from datetime import date
+from datetime import datetime
 
 app = Flask(__name__)
 
-# def marks_prediction():
-#     a=4
-#     return a
-def marks_prediction():
-    df_Makanan = pd.read_csv('/content/fix 02 - Makanan.csv')
-    df_Minuman = pd.read_csv('/content/fix 02 - Minuman.csv')
-    df_Rumah = pd.read_csv('/content/fix 03 - Rumah.csv')
-    df_Sandang = pd.read_csv('/content/fix 04 - Sandang.csv')
-    df_Transportasi = pd.read_csv('/content/fix 07 - Transportasi.csv')
+def sector_chosen(argument):
 
-    df_Makanan = df_Makanan[['KOTA MEULABOH','BULAN']]
-    df_Makanan.columns = ['y', 'ds']
+    df_Makanan = pd.read_csv('fix 02 - Makanan.csv')
+    df_Minuman = pd.read_csv('fix 02 - Minuman.csv')
+    df_Rumah = pd.read_csv('fix 03 - Rumah.csv')
+    df_Sandang = pd.read_csv('fix 04 - Sandang.csv')
+    df_Transportasi = pd.read_csv('fix 07 - Transportasi.csv')
 
-    df_Makanan.y = pd.to_datetime(df_Makanan.y)
+    switcher = {
+        "MAKANAN": df_Makanan,
+        "MINUMAN_TAK_BERALKOHOL": df_Minuman,        
+    }
+ 
+    return switcher.get(argument, "nothing")
+
+def marks_prediction(value,city, sector, start_date, end_date):
+    df_Makanan = pd.read_csv('fix 02 - Makanan.csv')
+    df_Minuman = pd.read_csv('fix 02 - Minuman.csv')
+    df_Rumah = pd.read_csv('fix 03 - Rumah.csv')
+    df_Sandang = pd.read_csv('fix 04 - Sandang.csv')
+    df_Transportasi = pd.read_csv('fix 07 - Transportasi.csv')
+
+    sector = sector_chosen(sector)
+
+    sector = sector[[city,'BULAN']]
+    sector.columns = ['y', 'ds']
+
+    sector.ds = pd.to_datetime(sector.ds)
 
     m = Prophet(interval_width=0.95, daily_seasonality=True)
-    model = m.fit(df_Makanan)
+    model = m.fit(sector)
 
     future = m.make_future_dataframe(periods=100,freq='M')
     forecast = m.predict(future)
 
-    forecast_value = forecast.loc[forecast.ds == '2018-01-01',['trend']].values[0]
+    # forecast_value = forecast.loc[forecast.ds == '2014-01-01',['yhat']].values[0]
 
-    return forecast_value
+    mask = (forecast['ds'] > '2021-10-31') & (forecast['ds'] <= '2021-12-31')
+    forecast_mask = forecast.loc[mask]
+
+    inflation_average_rate = (forecast_mask['yhat'].sum())/forecast_mask.shape[0]
+
+    value += inflation_average_rate*value
+    return value
 
 @app.route("/", methods = ["GET","POST"])
 def marks():
     mk=""
     if request.method == "POST":
-        marks_pred = marks_prediction()
-        # print(marks_pred)
+        
+        value = request.form['value']
+        city = request.form['city']
+        sector = request.form['sector_option']
+        start_date = date.today()
+        end_date = request.form['inflation_date_end']
+
+        value = float(value)
+
+        # start_date = datetime.strptime(start_date, '%m-%d-%Y').date()
+        # end_date = datetime.strptime(end_date, '%m-%d-%Y').date()
+        print(type(end_date))
+        marks_pred = marks_prediction(value,city, sector, start_date, end_date)
+        print(marks_pred)
         mk = marks_pred
     
     return render_template("index.html",my_marks = mk)
-
-# @app.route("/", methods = ['POST'])
-# def submit():
-#     # HTML -> py
-#     if request.method == "POST":
-        
-#         value = request.form['value']
-#         city = request.form['city']
-#         sector = request.form['sector']
-#         date = request.form['date']
-
-#         marks_pred = marks.marks_prediction()
-#         mk = marks_pred
-#     #py -> HTML
-#     return render_template("index.html" , mymarks = mk)
 
 if __name__== "__main__":
     app.run(debug = True)

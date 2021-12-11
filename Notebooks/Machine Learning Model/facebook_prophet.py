ba0f91b33be573da597bd5df73fbaa797e340789
Farel Arden <farelarden13@gmail.com>
2021-12-11 10:57:47 +0700

    Add files via upload

--- Notebooks/Machine Learning Model/facebook_prophet.py
+++ Notebooks/Machine Learning Model/facebook_prophet.py
@@ -30,14 +30,20 @@ df_Makanan= df_Makanan.drop(['URUTAN'], axis=1)
 
 df_Makanan.head()
 
+df_Makanan.dtypes
+
 df_Makanan = df_Makanan[['KOTA MEULABOH','BULAN']]
 df_Makanan.columns = ['y', 'ds']
 
 df_Makanan.head()
 
+df_Makanan.at[0,'y']
+
 df_Makanan.dtypes
 
-df_Makanan.y = pd.to_datetime(df_Makanan.y)
+df_Makanan.ds = pd.to_datetime(df_Makanan.ds)
+
+df_Makanan.dtypes
 
 df_Makanan.head()
 
@@ -59,7 +65,48 @@ plt2 = m.plot_components(forecast)
 
 forecast.dtypes
 
-forecast_value = forecast.loc[forecast.ds == '2018-01-01',['trend']].values[0]
+forecast_value = forecast.loc[forecast.ds == '2014-01-01',['yhat']].values[0]
 
 forecast_value
+
+# df_Makanan.head()
+df_Makanan.loc[df_Makanan.ds == '2014-01-01',['y']].values[0]
+
+df_Makanan.head()
+
+mask = (forecast['ds'] > '2021-10-31') & (forecast['ds'] <= '2021-12-31')
+forecast_mask = forecast.loc[mask]
+
+forecast_mask
+
+inflation_average_rate = (forecast_mask['yhat'].sum())/forecast_mask.shape[0]
+inflation_average_rate
+
+from datetime import date
+from datetime import datetime
+
+end_date="2019-01-01"
+
+end_date = pd.to_datetime(end_date)
+
+print(end_date)
+
+print(type(end_date))
+
+start_date = date.today()
+
+start_date = pd.to_datetime(start_date)
+
+mask = (forecast['ds'] > end_date) & (forecast['ds'] <= start_date)
+forecast_mask = forecast.loc[mask]
+
+forecast_mask
+
+value = 100
+
+forecast_mask['value'] = value + forecast_mask['yhat']*value*0.01
+
+forecast_mask.head()
+
+forecast_mask.dtypes
 
